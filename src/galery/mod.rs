dcff2ffd1e6f8b5746d7049ae1efa7d2660641cc
Thomas Loubiou <thomas.loubiou@mirakl.com>
2025-01-09 21:34:24 +0100

    fix: rename fields

--- src/galery/mod.rs
+++ src/galery/mod.rs
@@ -1,5 +1,8 @@
-use serde::Deserialize;
 use std::io::Cursor;
+
+use bytes::Bytes;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
 
 use image::ImageReader;
 
@@ -8,48 +11,107 @@ pub trait Galery {
 }
 
 pub struct ImmichGalery {
-    base_url: String,
-    api_key: String,
+    client: ImmichClient,
 }
 
 impl ImmichGalery {
     pub fn new(base_url: impl AsRef<str>, api_key: impl AsRef<str>) -> Self {
         Self {
-            base_url: base_url.as_ref().into(),
-            api_key: api_key.as_ref().into(),
+            client: ImmichClient::new(base_url, api_key),
         }
     }
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-struct WithId {
-    id: String,
-    original_file_name: String,
-    #[serde(rename = "type")]
-    type_: String,
+struct AssetResponse {
+    pub id: String,
+    // pub original_file_name: String,
+    // pub r#type: AssetType,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum AssetType {
+    IMAGE,
+    VIDEO,
+    AUDIO,
+    OTHER,
+}
+
+#[derive(Serialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct SearchRandomRequest {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    library_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    size: Option<u16>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    r#type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    person_ids: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    with_people: Option<bool>,
 }
 
 impl Galery for ImmichGalery {
     fn get_next_image(&mut self) -> image::DynamicImage {
-        let next = minreq::get(format!("{}/api/assets/random", self.base_url))
-            .with_header("x-api-key", &self.api_key)
-            .send()
-            .unwrap();
-        let mut next = next.json::<Vec<WithId>>().unwrap();
+        let mut next = self.client.search_random(SearchRandomRequest::default());
         let img = next.pop().unwrap();
-        let img_data = minreq::get(format!(
-            "{}/api/assets/{}/thumbnail?size=preview",
-            self.base_url, img.id
-        ))
-        .with_header("x-api-key", &self.api_key)
-        .send()
-        .unwrap()
-        .into_bytes();
+        let img_data = self.client.view_assets(img.id);
         ImageReader::new(Cursor::new(&img_data))
             .with_guessed_format()
             .unwrap()
             .decode()
             .unwrap()
+    }
+}
+
+struct ImmichClient {
+    base_url: String,
+    api_key: String,
+    client: reqwest::blocking::Client,
+}
+
+impl ImmichClient {
+    pub fn new(base_url: impl AsRef<str>, api_key: impl AsRef<str>) -> Self {
+        Self {
+            base_url: base_url.as_ref().into(),
+            api_key: api_key.as_ref().into(),
+            client: reqwest::blocking::Client::new(),
+        }
+    }
+
+    pub fn search_random(&self, query: SearchRandomRequest) -> Vec<AssetResponse> {
+        self.post("search/random")
+            .json(&query)
+            .header("Accept", "application/json")
+            .send()
+            .unwrap()
+            .json()
+            .unwrap()
+    }
+
+    pub fn view_assets(&self, id: String) -> Bytes {
+        self.get(format!("assets/{}/thumbnail?size=preview", id))
+            .send()
+            .unwrap()
+            .bytes()
+            .unwrap()
+    }
+
+    fn post(&self, path: impl AsRef<str>) -> reqwest::blocking::RequestBuilder {
+        self.request(Method::POST, path)
+    }
+
+    fn get(&self, path: impl AsRef<str>) -> reqwest::blocking::RequestBuilder {
+        self.request(Method::GET, path)
+    }
+
+    fn request(&self, method: Method, path: impl AsRef<str>) -> reqwest::blocking::RequestBuilder {
+        let url = format!("{}/api/{}", self.base_url, path.as_ref());
+        self.client
+            .request(method, url)
+            .header("x-api-key", &self.api_key)
     }
 }
