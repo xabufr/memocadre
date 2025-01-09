f82b7a266995ca98e574d507669dba8ce6606aac
Thomas Loubiou <xabufr@gmail.com>
2025-01-09 22:01:19 +0100

    chore: delete useless code/ressources

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -12,95 +12,6 @@ use winit::event::WindowEvent;
 use winit::event_loop::ActiveEventLoop;
 use winit::window::WindowId;
 
-pub mod camera;
-
-/// Returns a vertex buffer that should be rendered as `TrianglesList`.
-pub fn load_wavefront(
-    display: &Display<WindowSurface>,
-    data: &[u8],
-) -> glium::vertex::VertexBufferAny {
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 3],
-        normal: [f32; 3],
-        texture: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position, normal, texture);
-
-    let mut data = ::std::io::BufReader::new(data);
-    let data = obj::ObjData::load_buf(&mut data).unwrap();
-
-    let mut vertex_data = Vec::new();
-
-    for object in data.objects.iter() {
-        for polygon in object.groups.iter().flat_map(|g| g.polys.iter()) {
-            match polygon {
-                obj::SimplePolygon(indices) => {
-                    for v in indices.iter() {
-                        let position = data.position[v.0];
-                        let texture = v.1.map(|index| data.texture[index]);
-                        let normal = v.2.map(|index| data.normal[index]);
-
-                        let texture = texture.unwrap_or([0.0, 0.0]);
-                        let normal = normal.unwrap_or([0.0, 0.0, 0.0]);
-
-                        vertex_data.push(Vertex {
-                            position,
-                            normal,
-                            texture,
-                        })
-                    }
-                }
-            }
-        }
-    }
-
-    glium::vertex::VertexBuffer::new(display, &vertex_data)
-        .unwrap()
-        .into()
-}
-
-pub fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    let s = [
-        up[1] * f[2] - up[2] * f[1],
-        up[2] * f[0] - up[0] * f[2],
-        up[0] * f[1] - up[1] * f[0],
-    ];
-
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    let u = [
-        f[1] * s_norm[2] - f[2] * s_norm[1],
-        f[2] * s_norm[0] - f[0] * s_norm[2],
-        f[0] * s_norm[1] - f[1] * s_norm[0],
-    ];
-
-    let p = [
-        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
-    ];
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
-}
-
 pub trait ApplicationContext {
     fn draw_frame(&mut self, _display: &Display<WindowSurface>) {}
     fn new(display: &Display<WindowSurface>) -> Self;
@@ -283,20 +194,6 @@ impl<T: ApplicationContext + 'static> State<T> {
             state: None,
             visible: true,
             close_promptly: false,
-        };
-        let result = event_loop.run_app(&mut app);
-        result.unwrap();
-    }
-
-    /// Create a context and draw a single frame
-    pub fn run_once(visible: bool) {
-        let event_loop = winit::event_loop::EventLoop::builder()
-            .build()
-            .expect("event loop building");
-        let mut app = App::<T> {
-            state: None,
-            visible,
-            close_promptly: true,
         };
         let result = event_loop.run_app(&mut app);
         result.unwrap();
