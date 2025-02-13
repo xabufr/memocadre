fc9169e4c6369e55912b3fc6e2f86282b4985a8d
Thomas Loubiou <xabufr@gmail.com>
2025-02-13 20:11:54 +0100

    fix: add missing tools

--- shell.nix
+++ shell.nix
@@ -1,34 +1,36 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import <nixpkgs> {};
 in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    # rustc
-    # cargo
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      # rustc
+      # cargo
 
-    libGL
-    xorg.libX11
-    xorg.libXi
-    xorg.libXrender
-    xorg.libXcursor
-    libxkbcommon
-    mesa
-    cargo-deb
-  ];
-  # TODO: add rust nightly for fmt
-  shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
-      with pkgs;
-      pkgs.lib.makeLibraryPath [
-        libGL
-        libxkbcommon
-        wayland
-        xorg.libX11
-        xorg.libXrender
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libxcb
-      ]
-    }"
-  '';
-}
+      libGL
+      xorg.libX11
+      xorg.libXi
+      xorg.libXrender
+      xorg.libXcursor
+      libxkbcommon
+      mesa
+      cargo-deb
+      cargo-cross
+      just
+    ];
+    # TODO: add rust nightly for fmt
+    shellHook = ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+        with pkgs;
+          pkgs.lib.makeLibraryPath [
+            libGL
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXrender
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libxcb
+          ]
+      }"
+    '';
+  }
