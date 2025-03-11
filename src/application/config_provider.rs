61926b1728417932e8e680ac99f7656f1826a946
Thomas Loubiou <xabufr@gmail.com>
2025-03-11 22:01:14 +0100

    feat: add dynamic settings loading

--- src/application/config_provider.rs
+++ src/application/config_provider.rs
@@ -1,20 +1,39 @@
+use std::path::PathBuf;
+
 use crate::configuration::{AppSources, Settings};
 use anyhow::{Context, Result};
 use config::Config;
+use directories::ProjectDirs;
+use log::warn;
 
-pub struct ConfigProvider {}
+pub struct ConfigProvider {
+    settings_path: Option<PathBuf>,
+}
 
 impl ConfigProvider {
     pub fn new() -> ConfigProvider {
-        ConfigProvider {}
+        let settings_path = std::env::var("DYNAMIC_SETTINGS_PATH");
+        let settings_path = if let Ok(path) = settings_path {
+            Some(PathBuf::from(path))
+        } else if let Some(proj_dirs) = ProjectDirs::from("com", "xabufr", "photokiosk") {
+            Some(proj_dirs.config_dir().join("settings.yaml"))
+        } else {
+            warn!("Cannot find settings path");
+            None
+        };
+        ConfigProvider { settings_path }
     }
 
     pub fn load_settings(&self) -> Result<Settings> {
         let config_path = std::env::var("SETTINGS_PATH").unwrap_or("settings".to_string());
-        let settings = Config::builder()
-            .add_source(::config::File::with_name(&config_path))
-            .build()
-            .context("Cannot parse configuration")?;
+        let mut builder = Config::builder().add_source(::config::File::with_name(&config_path));
+
+        if let Some(settings_path) = &self.settings_path {
+            let source = ::config::File::from(settings_path.as_path()).required(false);
+            builder = builder.add_source(source);
+        }
+
+        let settings = builder.build().context("Cannot parse configuration")?;
         let config: Settings = settings
             .try_deserialize()
             .context("Cannot deserialize settings")?;
