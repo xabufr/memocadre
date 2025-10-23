cf2ff828752de2377008338318c40b92f5958a4a
Thomas Loubiou <xabufr@gmail.com>
2025-10-23 23:17:55 +0200

    test: load_settings

--- src/application/config_provider.rs
+++ src/application/config_provider.rs
@@ -8,13 +8,14 @@ use log::{debug, warn};
 use crate::configuration::{AppConfig, Settings, SettingsPatch};
 
 pub struct ConfigProvider {
-    settings_path: Option<PathBuf>,
+    dynamic_settings_path: Option<PathBuf>,
+    settings_path: String,
 }
 
 impl ConfigProvider {
     pub fn new() -> ConfigProvider {
         let settings_path = std::env::var("DYNAMIC_SETTINGS_PATH");
-        let settings_path = if let Ok(path) = settings_path {
+        let dynamic_settings_path = if let Ok(path) = settings_path {
             Some(PathBuf::from(path))
         } else if let Some(proj_dirs) = ProjectDirs::from("com", "xabufr", "photokiosk") {
             Some(proj_dirs.config_dir().join("settings.yaml"))
@@ -22,14 +23,19 @@ impl ConfigProvider {
             warn!("Cannot find settings path");
             None
         };
-        ConfigProvider { settings_path }
+
+        let settings_path = std::env::var("SETTINGS_PATH").unwrap_or("settings".to_string());
+        ConfigProvider {
+            dynamic_settings_path,
+            settings_path: settings_path,
+        }
     }
 
     pub fn load_settings(&self) -> Result<Settings> {
-        let config_path = std::env::var("SETTINGS_PATH").unwrap_or("settings".to_string());
-        let mut builder = Config::builder().add_source(::config::File::with_name(&config_path));
+        let mut builder =
+            Config::builder().add_source(::config::File::with_name(&self.settings_path));
 
-        if let Some(settings_path) = &self.settings_path {
+        if let Some(settings_path) = &self.dynamic_settings_path {
             debug!("Loading settings from {:?}", settings_path);
             let source = ::config::File::from(settings_path.as_path()).required(false);
             builder = builder.add_source(source);
@@ -57,5 +63,68 @@ impl ConfigProvider {
     pub fn save_settings_override(&self, settings: &SettingsPatch) -> Result<()> {
         // TODO implement me!
         anyhow::bail!("Not implemented");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use googletest::{
+        expect_that, gtest,
+        matchers::matches_pattern,
+        prelude::{approx_eq, eq},
+    };
+    use temp_dir::TempDir;
+
+    use super::ConfigProvider;
+
+    #[gtest]
+    fn test_load_settings() {
+        let settings = "";
+        let settings_dir = gen_settings_from_str(settings).unwrap();
+
+        let provider = ConfigProvider {
+            dynamic_settings_path: None,
+            settings_path: settings_dir
+                .path()
+                .join("settings.yaml")
+                .to_str()
+                .unwrap()
+                .to_string(),
+        };
+        let settings = provider.load_settings().unwrap();
+        expect_that!(settings.transition_duration, eq(Duration::from_millis(500)));
+    }
+
+    #[gtest]
+    fn test_load_missing_dynamic_file() {
+        let settings = "";
+        let settings_dir = gen_settings_from_str(settings).unwrap();
+        let empty_dir = empty_dir().unwrap();
+
+        let provider = ConfigProvider {
+            dynamic_settings_path: Some(empty_dir.path().join("missing.yaml")),
+            settings_path: settings_dir
+                .path()
+                .join("settings.yaml")
+                .to_str()
+                .unwrap()
+                .to_string(),
+        };
+        let settings = provider.load_settings().unwrap();
+        expect_that!(settings.transition_duration, eq(Duration::from_millis(500)));
+    }
+
+    fn gen_settings_from_str(s: &str) -> Result<TempDir, anyhow::Error> {
+        let temp_dir = TempDir::new().unwrap();
+        let settings_path = temp_dir.path().join("settings.yaml");
+        std::fs::write(&settings_path, s).unwrap();
+        Ok(temp_dir)
+    }
+
+    fn empty_dir() -> Result<TempDir, anyhow::Error> {
+        let temp_dir = TempDir::new().unwrap();
+        Ok(temp_dir)
     }
 }
